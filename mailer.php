5e53248368dd475ffa54eb0f2dd537501d133eea
Celand <celand@users.noreply.github.com>
2017-10-16 16:21:08 +0200

    Update mailer.php
    update recipient email

--- mailer.php
+++ mailer.php
@@ -22,7 +22,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "contact@markups.io";
+        $recipient = "summit@opensips.org";
 
         // Set the email subject.
         $subject = "New contact from $name";
