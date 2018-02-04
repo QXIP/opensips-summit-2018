b0e1a33e2a6c556cb1e1f5a058613af478225d15
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2018-02-04 17:58:57 +0100

    Update conference.js

--- conference.js
+++ conference.js
@@ -1,70 +1,72 @@
 var conference = {
     speakers: [
 	{
-            "name": "Bogdan Andrei Iancu",
+            "id": 1,
+	    "name": "Bogdan Andrei Iancu",
             "title": "OpenSIPS Founder",
             "image": "http://i.imgur.com/c0q8TjB.png",
             "twitter": "@opensips",
             "presentation": "OpenSIPS Keynote"
         },
         {
-            "name": "Liviu Chiurcu",
+            "id": 2,
+	    "name": "Liviu Chiurcu",
             "title": "Sr. OpenSIPS Developer",
             "image": "http://i.imgur.com/LmSk7gI.jpg",
             "twitter": "@liviuchirchu",
             "presentation": ""
         },
         {
-            "name": "Razvan Crainea",
+            "id": 3, "name": "Razvan Crainea",
             "title": "Sr. OpenSIPS Developer",
-            "image": "http: //i.imgur.com/YbMlg4m.png",
+            "image": "http://i.imgur.com/YbMlg4m.png",
             "twitter": "@razvancrainea",
             "presentation": ""
         },
         {
-            "name": "Alex Goulis",
+            "id": 4, "name": "Alex Goulis",
             "title": "CEO @RateTel",
             "image": "http://i.imgur.com/FvHRqNc.png",
             "twitter": "",
             "presentation": ""
         },
         {
-            "name": "Lorenzo Mangani",
+            "id": 5, "name": "Lorenzo Mangani",
             "title": "CEO @QXIP",
             "image": "http://i.imgur.com/XJS0Ihj.png",
             "twitter": "@qxip",
             "presentation": "HEP in NodeJS"
         },
 	{
-            "name": "Peter Kelly",
+            "id": 6, "name": "Peter Kelly",
             "title": "CEO @SourceVox",
             "image": "https://user-images.githubusercontent.com/1423657/31589533-a7cf4f5e-b203-11e7-8a52-bf2f2cfdf72a.png",
             "twitter": "",
             "presentation": ""
         },
         {
-            "name": "Flavio Goncalves",
+            "id": 7, "name": "Flavio Goncalves",
             "title": "CEO @SIPPulse",
             "image": "http://i.imgur.com/5PS7U7r.png",
             "twitter": "",
             "presentation": ""
         },
         {
-            "name": "Dan Christian Bogos",
+            "id": 8, "name": "Dan Christian Bogos",
             "title": "Founder @ITSysCom",
             "image": "http://i.imgur.com/2D2AjWr.png",
             "twitter": "",
             "presentation": ""
         },
         {
-            "name": "Lorenzo Miniero",
+            "id": 9, "name": "Lorenzo Miniero",
             "title": "Co-Founcer @Meetecho",
             "image": "http://i.imgur.com/4rfCiZB.png",
             "twitter": "",
             "presentation": ""
         },
         {
-            "name": "Alexandr Dubovikov",
+            "id": 10, "name": "Alexandr Dubovikov",
             "title": "CTO @QXIP",
             "image": "http://i.imgur.com/F83gMim.png",
             "twitter": "",
