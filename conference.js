eae0de4b9c0d738f14ea530de6bc565ba6bb78f1
Celand <celand@users.noreply.github.com>
2018-02-05 18:45:24 +0100

    Update conference.js

--- conference.js
+++ conference.js
@@ -4,14 +4,14 @@ var conference = {
             "id": 1, "name": "Bogdan Andrei Iancu",
             "title": "OpenSIPS Founder",
             "image": "http://i.imgur.com/c0q8TjB.png",
-            "twitter": "https://twitter.com/opensips",
+            "twitter": "@opensips",
             "presentation": "OpenSIPS Keynote"
         },   
         {
             "id": 2, "name": "Liviu Chiurcu",
             "title": "Sr. OpenSIPS Developer",
             "image": "http://i.imgur.com/LmSk7gI.jpg",
-            "twitter": "liviuchirchu",
+            "twitter": "@liviuchirchu",
             "presentation": ""
         },
         {
@@ -25,63 +25,63 @@ var conference = {
             "id": 4, "name": "Alex Goulis",
             "title": "CEO @RateTel",
             "image": "http://i.imgur.com/FvHRqNc.png",
-            "twitter": "ratetel",
+            "twitter": "@ratetel",
             "presentation": ""
 	},  
         {
             "id": 5, "name": "Lorenzo Mangani",
             "title": "CEO @QXIP",
             "image": "http://i.imgur.com/XJS0Ihj.png",
-            "twitter": "https://twitter.com/qxip",
+            "twitter": "@qxip",
             "presentation": "HEP in NodeJS"
         },
 	{
             "id": 6, "name": "Peter Kelly",
             "title": "CEO @SourceVox",
             "image": "https://user-images.githubusercontent.com/1423657/31589533-a7cf4f5e-b203-11e7-8a52-bf2f2cfdf72a.png",
-            "twitter": "https://twitter.com/p3k4y",
+            "twitter": "@p3k4y",
             "presentation": ""
         },
         {
             "id": 7, "name": "Flavio Goncalves",
             "title": "CEO @SIPPulse",
             "image": "http://i.imgur.com/5PS7U7r.png",
-            "twitter": "opensips",
+            "twitter": "@SipPulse",
             "presentation": ""
         },
         {
             "id": 8, "name": "Dan Christian Bogos",
             "title": "Founder @CGRates",
             "image": "http://i.imgur.com/2D2AjWr.png",
-            "twitter": "danbogos",
+            "twitter": "@danbogos",
             "presentation": ""
         },
         {
             "id": 9, "name": "Lorenzo Miniero",
             "title": "Co-Founder @Meetecho",
             "image": "http://i.imgur.com/4rfCiZB.png",
-            "twitter": "https://twitter.com/elminiero",
+            "twitter": "@elminiero",
             "presentation": "Troubleshooting and monitoring Janus: a HEPIC journey!"
         },
         {
             "id": 10, "name": "Alexandr Dubovikov",
             "title": "CTO @QXIP",
             "image": "http://i.imgur.com/F83gMim.png",
-            "twitter": "https://twitter.com/adubovikov",
+            "twitter": "@sipcapture",
             "presentation": ""
         },    
 	{
             "id": 11, "name": "Giovanni Maruzzelli",
             "title": "OpenTelecom.it",
             "image": "http://i.imgur.com/heUiUgH.jpg?1",
-            "twitter": "https://twitter.com/gmaruzz",
+            "twitter": "@gmaruzz",
             "presentation": "FreeSWITCH clustering with OpenSIPS (done well)"
         },    
 	{
 	    "id":12,"name":"Maxim Sobolev",
 	    "title":"Sippy Software, Inc.", 
 	    "image":"http://sobomax.sippysoft.com/2efb44_9d016a626c1b0fded8397399c5401f6b.png",
-	    "twitter":"https://twitter.com/sobomax",
+	    "twitter":"@sobomax",
 	    "presentation":"SIP in the age of IoT"
 	 },
 	    
