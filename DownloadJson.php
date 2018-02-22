bb418b0d88fff08166858428c83c24690521c284
Voicenter <shlomi@voicenter.com>
2018-02-22 18:18:18 +0200

    Voicenter Small Help  (#4)
    * Yeal Changes
    
    * Vue setup + data.json with conf data
    
    * Load Data From Json
    
    * Download joson from google script :
    php  test.php > data.json
    
    * Finish VUE Spekers
    
    * Name
    
    * Moved "Speakers" as "Papers" panel and place it as last
    
    * Remove statis listing of Bogdan
    
    * Fixed missing php closing tag
    
    * Move paper title in the bar and remive it from expanded body
    
    * Updated data
    
    * v-for dynamic id's added for collapse bug
    
    * added filter - speaker with empty name in JSON will not be added to HTML accordion
    
    * Fix the display photo for the spearker
    
    * dynamic speakers-slide added . data.json format edited
    
    * speakers slider
    
    * speakers added to data.json ,speakers slider changed from static to dynamic
    
    * SPEAKERSSLIDE data merged with SPEAKERS .
    
    * no profile photo added -on empty photo from data.json , on empty twitter link hides the icon

---
+++ DownloadJson.php
@@ -0,0 +1,11 @@
+<?php
+$APPS_SCRIPT_URL = 'https://script.google.com/a/starkey.co.il/macros/s/AKfycbxwJg0CGHqykOLtQr_afHCAvrpupsLFG2FyWGsaVFLxOmFnRXM/exec';
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $APPS_SCRIPT_URL);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_TIMEOUT, 20);
+$content = trim(curl_exec($ch));
+curl_close($ch);
+?>
